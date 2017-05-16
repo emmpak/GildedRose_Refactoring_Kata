cdd7a7cfd9a9fb10650e90dff2451e4beee1b39b
emmpak <eiliev05@yahoo.com>
2017-05-16 11:58:10 +0100

    Add decrease test past the sell by date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,20 +1,25 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  let(:elixir) { Item.new(name="Elixir of the Mongoose", sell_in=5, quality=1) }
+  let(:elixir) { Item.new(name="Elixir of the Mongoose", sell_in=5, quality=10) }
   let(:brie) { Item.new(name="Aged Brie", sell_in=10, quality=49) }
   let(:items) { [elixir, brie] }
   subject(:gilded_rose) { described_class.new(items) }
 
-  context 'regular products' do
+  context 'common features for the products' do
 
-    it 'quality decreases by 1 with each day until quality is 0' do
+    it 'quality decreases by 1 with each day until it reaches 0 or the sell by date has not been passed' do
       expect { gilded_rose.update_quality }.to change { elixir.quality }.by -1
     end
 
     it 'quality cannot be a negative value' do
-      gilded_rose.update_quality
+      10.times { gilded_rose.update_quality }
       expect { gilded_rose.update_quality }.not_to change { elixir.quality }
+    end
+
+    it 'qulity decreases by 2 with each day once the sell by date has passed' do
+      5.times { gilded_rose.update_quality }
+      expect { gilded_rose.update_quality }.to change { elixir.quality }.by -2
     end
   end
 
