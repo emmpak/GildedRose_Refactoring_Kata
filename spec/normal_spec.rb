bf31d3fb1424a768da989f3b95086158d62fbeca
emmpak <eiliev05@yahoo.com>
2017-05-17 12:20:20 +0100

    Add Normal class

--- spec/normal_spec.rb
+++ spec/normal_spec.rb
@@ -1,18 +1,20 @@
-descrbe Normal do
+require 'normal'
+
+describe Normal do
   let(:elixir) { Item.new(name="Elixir of the Mongoose", sell_in=5, quality=10) }
-  subject(item) { described_class.new(elixir) }
+  subject(:item) { described_class.new(elixir) }
 
   it 'quality decreases by 1 with each day until it reaches 0 or the sell by date has not been passed' do
-    expect { gilded_rose.update_quality }.to change { elixir.quality }.by -1
+    expect { item.tick }.to change { elixir.quality }.by -1
   end
 
   it 'quality cannot be a negative value' do
-    10.times { gilded_rose.update_quality }
-    expect { gilded_rose.update_quality }.not_to change { elixir.quality }
+    10.times { item.tick }
+    expect { item.tick }.not_to change { elixir.quality }
   end
 
   it 'quality decreases by 2 with each day once the sell by date has passed' do
-    5.times { gilded_rose.update_quality }
-    expect { gilded_rose.update_quality }.to change { elixir.quality }.by -2
+    5.times { item.tick }
+    expect { item.tick }.to change { elixir.quality }.by -2
   end
 end
