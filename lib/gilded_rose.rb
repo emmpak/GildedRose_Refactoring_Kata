b4aba55e48d7a352697379a9588870e0377309e4
emmpak <eiliev05@yahoo.com>
2017-05-17 13:41:56 +0100

    Remove if statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,78 +6,25 @@ class GildedRose
     @items = items
   end
 
-  def normal_tick(item)
-    Normal.new(item).tick
-  end
-
-  def brie_tick(item)
-    Brie.new(item).tick
-  end
-
   def sulfuras_tick(item)
-  end
-
-  def backstage_tick(item)
-    Concert.new(item).tick
   end
 
   def update_quality
     items.each do |item|
       case item.name
       when "Aged Brie"
-        brie_tick(item)
+        Brie.new(item).tick
       when "Sulfuras, Hand of Ragnaros"
         sulfuras_tick(item)
       when "Backstage passes to a TAFKAL80ETC concert"
-        backstage_tick(item)
+        Concert.new(item).tick
       else
-        normal_tick(item)
+        Normal.new(item).tick
       end
     end
   end
 end
 
-      # if item.name == 'Elixir of the Mongoose'
-      #   return normal_tick(item)
-      # end
-
-  #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-  #       item.quality = item.quality - 1 if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros"
-  #     else
-  #       if item.quality < 50
-  #         item.quality = item.quality + 1
-  #         if item.name == "Backstage passes to a TAFKAL80ETC concert"
-  #           if item.sell_in < 11 && item.quality < 50
-  #               item.quality = item.quality + 1
-  #           end
-  #           if item.sell_in < 6 && item.quality < 50
-  #               item.quality = item.quality + 1
-  #           end
-  #         end
-  #       end
-  #     end
-  #     if item.name != "Sulfuras, Hand of Ragnaros"
-  #       item.sell_in = item.sell_in - 1
-  #     end
-  #     if item.sell_in < 0
-  #       if item.name != "Aged Brie"
-  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
-  #           if item.quality > 0
-  #             if item.name != "Sulfuras, Hand of Ragnaros"
-  #               item.quality = item.quality - 1
-  #             end
-  #           end
-  #         else
-  #           item.quality = item.quality - item.quality
-  #         end
-  #       else
-  #         if item.quality < 50
-  #           item.quality = item.quality + 1
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
 
 class Item
   attr_accessor :name, :sell_in, :quality
