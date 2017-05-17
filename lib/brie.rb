a8e43eafddd5840f3f66759bf7b1aea05c241226
emmpak <eiliev05@yahoo.com>
2017-05-17 12:35:20 +0100

    Add Brie class

---
+++ lib/brie.rb
@@ -0,0 +1,15 @@
+class Brie
+
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def tick
+    item.sell_in -= 1
+    return if item.quality >= 50
+    item.quality += 1
+    item.quality += 1 if item.sell_in <= 0
+  end
+end
