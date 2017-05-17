0e53fefcb7b53cf9cda9d83423ae4bc5d4f9de5b
emmpak <eiliev05@yahoo.com>
2017-05-17 12:23:23 +0100

    Add brie_spec file and tests

---
+++ spec/brie_spec.rb
@@ -0,0 +1,19 @@
+describe Brie do
+  let(:item) { Item.new(name="Aged Brie", sell_in=10, quality=35) }
+  subject(:brie) { described_class.new(item) }
+
+  it 'quality increases with time' do
+    brie.tick
+    expect { brie.tick }.to change { brie.quality }.by 1
+  end
+
+  it 'quality increases by 2 once the sell by date has passed' do
+    10.times { brie.tick }
+    expect { brie.tick }.to change { brie.quality }.by 2
+  end
+
+  it 'cannot have a quality value of more than 50' do
+    15.times { brie.tick }
+    expect { brie.tick }.not_to change { brie.quality }
+  end
+end
