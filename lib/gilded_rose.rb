6709f6ef46414cf5e185af9773ac112328e81f2e
emmpak <eiliev05@yahoo.com>
2017-05-17 14:13:27 +0100

    Put Item class in a separate file

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -23,18 +23,3 @@ class GildedRose
     items.each { |item| klass_for(item.name).new(item).tick }
   end
 end
-
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
-end
